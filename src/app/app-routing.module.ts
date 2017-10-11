5cee7f570a2bf7c9e7cb72e912a6d98337141f72
AlfredoSebastiao <stiaoalfredo@gmail.com>
2017-10-11 18:09:11 +0200

    Instalacao e configuracao do bootstrapmaterial desig

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {  RouterModule, Routes} from "@angular/router";
+const appRoutes: Routes = [
+  {path: '',component: }
+];
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  exports:[RouterModule]
+})
+export class AppRoutingModule { }
