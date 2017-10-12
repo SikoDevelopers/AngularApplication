2c581d3e8068ea7edfed8844ac75cd1f38a14530
AlfredoSebastiao <stiaoalfredo@gmail.com>
2017-10-12 17:27:12 +0200

    Retornando Valores

--- src/app/select/select.service.ts
+++ src/app/select/select.service.ts
@@ -5,4 +5,30 @@ export class SelectService {
 
   constructor() { }
 
+
+  getOpcoes() {
+    return [
+      {
+        'value' : 'Valor 1',
+        'option': 'Opcao 1'
+      },
+      {
+        'value' : 'Valor 2',
+        'option': 'Opcao 2'
+      },
+      {
+        'value' : 'Valor 3',
+        'option': 'Opcao 3'
+      },
+      {
+        'value' : 'Valor 4',
+        'option': 'Opcao 4'
+      },
+      {
+        'value' : 'Valor 5',
+        'option': 'Opcao 5'
+      },
+    ];
+  }
+
 }
