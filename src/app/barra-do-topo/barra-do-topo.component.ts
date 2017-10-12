fb471929cadd65d446fb5a9bf533c2caea42e58c
AlfredoSebastiao <stiaoalfredo@gmail.com>
2017-10-12 14:37:08 +0200

    Directiva hidden para barra do topo

--- src/app/barra-do-topo/barra-do-topo.component.ts
+++ src/app/barra-do-topo/barra-do-topo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-barra-do-topo',
@@ -7,15 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BarraDoTopoComponent implements OnInit {
   nomeDaApp : string = "SikoSSP"
-  constructor() { }
+  rota: string;
+  constructor(private _route: Router) { }
 
-  optionsSelect: Array<any>;
 
   ngOnInit() {
-    this.optionsSelect = [
-      { value: '1', label: 'Option 1' },
-      { value: '2', label: 'Option 2' },
-      { value: '3', label: 'Option 3' },
-    ];
+    this.rota = this._route.url;
   }
 }
