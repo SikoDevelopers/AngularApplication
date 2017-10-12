2d45d643dc6b0de69f9ab5684e42961939da886b
AlfredoSebastiao <stiaoalfredo@gmail.com>
2017-10-12 07:10:27 +0200

    Criacao do componente para login

--- src/app/publico-geral/publico-geral.module.ts
+++ src/app/publico-geral/publico-geral.module.ts
@@ -4,12 +4,13 @@ import { PaginaInicialComponent } from './pagina-inicial/pagina-inicial.componen
 import {BarraDoTopoModule} from '../barra-do-topo/barra-do-topo.module';
 import { ListaDeTrabalhosComponent } from './lista-de-trabalhos/lista-de-trabalhos.component';
 import { TrabalhoComponent } from './lista-de-trabalhos/trabalho/trabalho.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   imports: [
     CommonModule,
     BarraDoTopoModule
   ],
-  declarations: [PaginaInicialComponent, ListaDeTrabalhosComponent, TrabalhoComponent]
+  declarations: [PaginaInicialComponent, ListaDeTrabalhosComponent, TrabalhoComponent, LoginComponent]
 })
 export class PublicoGeralModule { }
