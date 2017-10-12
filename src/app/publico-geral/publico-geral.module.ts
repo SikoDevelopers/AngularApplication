b7b8c1acf3c53588bf70edf9e160d9281b332337
AlfredoSebastiao <stiaoalfredo@gmail.com>
2017-10-12 13:32:32 +0200

    Criacao de Login-form component

--- src/app/publico-geral/publico-geral.module.ts
+++ src/app/publico-geral/publico-geral.module.ts
@@ -5,6 +5,7 @@ import { BarraDoTopoModule} from '../barra-do-topo/barra-do-topo.module';
 import { ListaDeTrabalhosComponent } from './lista-de-trabalhos/lista-de-trabalhos.component';
 import { TrabalhoComponent } from './lista-de-trabalhos/trabalho/trabalho.component';
 import { LoginComponent } from './login/login.component';
+import { LoginFormComponent } from './login/login-form/login-form.component';
 
 @NgModule({
   imports: [
@@ -15,7 +16,8 @@ import { LoginComponent } from './login/login.component';
     PaginaInicialComponent,
     ListaDeTrabalhosComponent,
     TrabalhoComponent,
-    LoginComponent
+    LoginComponent,
+    LoginFormComponent
   ]
 })
 export class PublicoGeralModule { }
