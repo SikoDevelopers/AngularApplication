35822b9e65041744e93c5bea90db3246555d3ee8
AlfredoSebastiao <stiaoalfredo@gmail.com>
2017-10-14 15:54:41 +0200

    Importacao do bootstrap

--- src/app/slider/slider.module.ts
+++ src/app/slider/slider.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SliderComponent } from './slider.component';
-
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MDBBootstrapModule
   ],
   declarations: [SliderComponent],
   exports:[SliderComponent]
