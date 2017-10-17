923f793acb653278ca93b7177e56b69842547892
AlfredoSebastiao <stiaoalfredo@gmail.com>
2017-10-17 19:07:13 +0200

    Modulo Estavel

--- src/app/docente/docente.routing.ts
+++ src/app/docente/docente.routing.ts
@@ -2,13 +2,13 @@ import { RouterModule, Routes} from '@angular/router';
 import {ModuleWithProviders} from '@angular/core';
 import {DocenteComponent} from './docente.component';
 import {EventosComponent} from "./conteudo/eventos/eventos.component";
-
+import { MeusSupervisionandosComponent } from './conteudo/meus-supervisionandos/meus-supervisionandos.component'
 
 const DOCENTE_ROTAS: Routes = [
     {path: '', component: DocenteComponent,
         children: [
+            {path:'docente/supervisandos',component: MeusSupervisionandosComponent},
             {path:'docente/eventos',component: EventosComponent}
-
         ],
 
 
