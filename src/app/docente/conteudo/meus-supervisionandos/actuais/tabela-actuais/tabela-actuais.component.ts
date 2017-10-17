8cab1a84e8dedb74ae578eaad8775b7e3f297cf2
AlfredoSebastiao <stiaoalfredo@gmail.com>
2017-10-17 15:20:25 +0200

    Criacao das constantes

--- src/app/docente/conteudo/meus-supervisionandos/actuais/tabela-actuais/tabela-actuais.component.ts
+++ src/app/docente/conteudo/meus-supervisionandos/actuais/tabela-actuais/tabela-actuais.component.ts
@@ -25,3 +25,9 @@ export class TabelaActuaisComponent implements OnInit {
   }
 
 }
+/** Constants used to fill up our data base. */
+const COLORS = ['maroon', 'red', 'orange', 'yellow', 'olive', 'green', 'purple',
+  'fuchsia', 'lime', 'teal', 'aqua', 'blue', 'navy', 'black', 'gray'];
+const NAMES = ['Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack',
+  'Charlotte', 'Theodore', 'Isla', 'Oliver', 'Isabella', 'Jasper',
+  'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'];
