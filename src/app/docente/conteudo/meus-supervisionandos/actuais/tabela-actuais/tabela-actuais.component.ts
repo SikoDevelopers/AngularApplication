d59e1dcedc43ce479b8f488039e97ff90a82244a
AlfredoSebastiao <stiaoalfredo@gmail.com>
2017-10-17 15:21:30 +0200

    Criacao de dados para simulacao

--- src/app/docente/conteudo/meus-supervisionandos/actuais/tabela-actuais/tabela-actuais.component.ts
+++ src/app/docente/conteudo/meus-supervisionandos/actuais/tabela-actuais/tabela-actuais.component.ts
@@ -38,3 +38,36 @@ export interface UserData {
   progress: string;
   color: string;
 }
+
+/** An example database that the data source uses to retrieve data for the table. */
+export class ExampleDatabase {
+  /** Stream that emits whenever the data has been modified. */
+  dataChange: BehaviorSubject<UserData[]> = new BehaviorSubject<UserData[]>([]);
+  get data(): UserData[] { return this.dataChange.value; }
+
+  constructor() {
+    // Fill up the database with 100 users.
+    for (let i = 0; i < 100; i++) { this.addUser(); }
+  }
+
+  /** Adds a new user to the database. */
+  addUser() {
+    const copiedData = this.data.slice();
+    copiedData.push(this.createNewUser());
+    this.dataChange.next(copiedData);
+  }
+
+  /** Builds and returns a new User. */
+  private createNewUser() {
+    const name =
+        NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
+        NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
+
+    return {
+      id: (this.data.length + 1).toString(),
+      name: name,
+      progress: Math.round(Math.random() * 100).toString(),
+      color: COLORS[Math.round(Math.random() * (COLORS.length - 1))]
+    };
+  }
+}
