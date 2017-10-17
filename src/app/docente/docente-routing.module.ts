8522e25252e48951ae18558809da6a26246a4e74
AlfredoSebastiao <stiaoalfredo@gmail.com>
2017-10-17 17:51:33 +0200

    Rota eventos criada

--- src/app/docente/docente-routing.module.ts
+++ src/app/docente/docente-routing.module.ts
@@ -1,9 +1,12 @@
 import { RouterModule, Routes} from '@angular/router';
 import {ModuleWithProviders} from '@angular/core';
 import {DocenteComponent} from './docente.component';
+import {EventosComponent} from "./conteudo/eventos/eventos.component";
+
 const COMISSAO_ROTAS: Routes = [
     {path: '', component: DocenteComponent,
         children: [
+            {path:'docente/eventos',component: EventosComponent}
 
         ],
 
