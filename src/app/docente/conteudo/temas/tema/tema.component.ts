ed2fd536b879223aaaf7d8efd7542b230e98541c
AlfredoSebastiao <stiaoalfredo@gmail.com>
2017-10-20 14:33:36 +0200

    Instaciacao do servico

--- src/app/docente/conteudo/temas/tema/tema.component.ts
+++ src/app/docente/conteudo/temas/tema/tema.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-tema',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tema.component.scss']
 })
 export class TemaComponent implements OnInit {
-
+  @Input() tema: any;
   constructor() { }
 
   ngOnInit() {
--- src/app/docente/conteudo/temas/temas.component.ts
+++ src/app/docente/conteudo/temas/temas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {TemaService} from "../../../service/tema.service";
 
 @Component({
   selector: 'app-temas',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./temas.component.scss']
 })
 export class TemasComponent implements OnInit {
-
-  constructor() { }
+  temas : any;
+  constructor(private _temaSevice: TemaService) { }
 
   ngOnInit() {
+    this.getTemas();
+  }
+
+  getTemas(){
+    this._temaSevice.getTema().subscribe(
+        resultado => { this.temas = resultado['tema'].data;},
+        error2 => {console.log("Error ao carregar "+error2)},
+        () =>{
+          console.log("Sucesso ao Carregar Temas");
+        }
+    );
   }
 
 }
