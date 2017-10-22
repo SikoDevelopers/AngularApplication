4791f1033c8fac35b587c51c8750368fe65cc5f0
herquiloidehele <herquiloide@gmail.com>
2017-10-22 12:15:01 +0200

    sss

---
+++ src/app/comissao-cientifica/conteudo/trabalhos/tabs-trabalhos/tabela-trabalhos-apenas/tabela-trabalhos-apenas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tabela-trabalhos-apenas',
+  templateUrl: './tabela-trabalhos-apenas.component.html',
+  styleUrls: ['./tabela-trabalhos-apenas.component.scss']
+})
+export class TabelaTrabalhosApenasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
