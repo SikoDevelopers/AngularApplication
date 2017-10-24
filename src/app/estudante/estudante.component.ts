fa4363d62e02589818338a2acf27d29c10f6fe8e
erciliomarques <erciliomarquesmanhica@gmail.com>
2017-10-24 21:18:21 +0200

    trabalhando com componentes estudante 1

--- src/app/estudante/estudante.component.ts
+++ src/app/estudante/estudante.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-estudante',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EstudanteComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private _router: Router) { }
+    rotas: string;
   ngOnInit() {
+    this.rotas=this._router.url;
   }
 
 }
