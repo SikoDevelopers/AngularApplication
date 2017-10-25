e0dab99eda102decdb8ac00b61b54433f72cff9f
erciliomarques <erciliomarquesmanhica@gmail.com>
2017-10-25 15:09:30 +0200

    working with student 6

--- src/app/service/ficheiros-trabalho.service.ts
+++ src/app/service/ficheiros-trabalho.service.ts
@@ -14,6 +14,11 @@ export class FicheirosTrabalhoService {
 
     }
 
+    getFicheirosTra(id): Observable<any>{
+        return this.http.get('http://127.0.0.1:8000/api/ficheiros_de_trabalho/id');
+
+    }
+
 
     saveFicheirosTrabalho(ficheirosTrabalho: FicheirosTrabalho): Observable<any>{
         const headers = new HttpHeaders({'Content-Type': 'application/json'});
