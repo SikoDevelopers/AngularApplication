d10f9f5e9f128ffdd3d7a8bca3bd0fb091cc7a83
erciliomarques <erciliomarquesmanhica@gmail.com>
2017-10-27 01:03:56 +0200

    ja visualiza o file from angular 5

--- src/app/estudante/trabalhos-submetidos/trabalhos-submetidos.component.ts
+++ src/app/estudante/trabalhos-submetidos/trabalhos-submetidos.component.ts
@@ -10,55 +10,84 @@ import {EstudanteService} from "../../service/estudante.service";
   styleUrls: ['./trabalhos-submetidos.component.scss']
 })
 export class TrabalhosSubmetidosComponent implements OnInit {
-    ficheiros;
+
+    ficheiros: Array<any>
     user;
+    estado;
+    estudante;
+    trabalho;
   constructor(private userService: UserService,
               private trabalhoService: TrabalhoService,
               private ficheiroService: FicheirosTrabalhoService,
-              private estudanteService: EstudanteService) { }
+              private estudanteService: EstudanteService,
+
+              ) { }
 
   ngOnInit() {
+      this.getUser();
+
+      // alert(this.user.id);
+      // alert('estudante'+this.estudante.id);
+      // alert('trabalho'+this.trabalho.id);
   }
 
-
-  getFiles(){
-
-  }
-
-
-
-    getEstudante(){
+    getUser(){
         const token = localStorage.getItem('token');
 
 
         this.userService.logado(token).subscribe(
             resultado=>{
-                this.user = resultado['user'];
-                alert(this.user.id);
-                this.estudanteService.getEstudantesByIdUser(this.user.id).subscribe(
-                    result=>{
-                     let estudante = result['estudante'];
-                      this.trabalhoService.getTrabalhosEstudante(estudante.id).subscribe(
-                          resul=>{
-                              let trabalho = resul['trabalho'];
-                              this.ficheiroService.getFicheirosTrabalho().subscribe(
-                                  res=>{
-
-                                  }
-                              )
-                          }
-                      )
-
-                    }
-                )
+                this.user = resultado;
 
             },
             error2 => {
-
             },
             ()=>{
                 console.log('user retrivied');
+                this.getEstudante();
+
             }
         );
     }
+
+
+    getEstudante(){
+
+                this.estudanteService.getEstudantesByIdUser(this.user.id).subscribe(
+                    result=>{
+                        this.estudante = result['estudante'];
+
+                    },
+                    (error)=>{
+                        console.log(error)
+                    },
+                    ()=>{
+                        this.getTrabalhoEstudante();
+
+                    }
+                )
+            }
+
+
+            getTrabalhoEstudante(){
+
+                this.trabalhoService.getTrabalhosEstudante(4).subscribe(
+                    resul=>{
+                        this.trabalho = resul['trabalho'];
+
+
+
+
+                    },
+                    (error)=>{
+                        console.log(error);
+                    },
+                    ()=>{
+                        this.ficheiros= this.trabalho.ficheiros_trabalhos;
+                        console.log('files '+this.ficheiros);
+
+                    }
+                )
+            }
+
 }
