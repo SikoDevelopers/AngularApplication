d852c6e1f022377354b8d2ebc013e8ca0eec48e6
AlfredoSebastiao <stiaoalfredo@gmail.com>
2017-10-26 16:35:11 +0200

    NgIf A causar Bug Removed

--- src/app/docente/conteudo/oponencia/lista-oponencia/tabela-oponencias/tabela-oponencias.component.ts
+++ src/app/docente/conteudo/oponencia/lista-oponencia/tabela-oponencias/tabela-oponencias.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-import {UserService} from "../../../../../service/user.service";
-import {DocenteService} from "../../../../../service/docente.service";
 @Component({
   selector: 'app-tabela-oponencias',
   templateUrl: './tabela-oponencias.component.html',
@@ -13,64 +11,13 @@ export class TabelaOponenciasComponent implements OnInit {
   user: any;
   docente: any;
 
-  constructor(private _userService:UserService,
-              private _docenteService:DocenteService
+  constructor(
   ){}
 
     ngOnInit() {
-//      this.getUser();
+
     }
 
-  getUser() {
-    let token = localStorage.getItem('token');
 
-    this._userService.logoado(token).subscribe(
-        resultado => {
-          this.user = resultado
-        },
-        error2 => {
-        },
-        () => {
-          this.getDocente(this.user.id);
-        }
-    );
-
-  }
-
-  getDocente(id) {
-    this._docenteService.getDocentePorId(id).subscribe(
-        resultado => {
-          this.docente = resultado['docente']
-        },
-        error2 => {
-          console.log("Error ao carregar Docente " + error2)
-        },
-        () => {
-          this.getEstudantes(this.docente.id);
-          console.log(this.docente);
-        }
-    );
-  }
-
-  getEstudantes(id:number){
-    this._docenteService.getOponencias(id).subscribe(
-        resultado => {
-          this.estudantes = resultado['oponencias_do_docente'];
-        },
-        error2 => {
-          console.log("Error ao carregar Oponencias ")
-        },
-        () => {
-
-        }
-    );
-  }
-  getEstado(is_aprovado){
-    if(is_aprovado){
-      return "Aprovado";
-    }
-    else
-      return "Nao Aprovado";
-  }
 }
 
