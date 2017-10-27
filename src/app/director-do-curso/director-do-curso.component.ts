72ab70b11fe62357db1f1f3d79e77c498dd7b739
AlfredoSebastiao <stiaoalfredo@gmail.com>
2017-10-27 12:23:17 +0200

    director component created

---
+++ src/app/director-do-curso/director-do-curso.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-director-do-curso',
+  templateUrl: './director-do-curso.component.html',
+  styleUrls: ['./director-do-curso.component.scss']
+})
+export class DirectorDoCursoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/director-do-curso/director-do-curso.module.ts
+++ src/app/director-do-curso/director-do-curso.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DirectorDoCursoComponent } from './director-do-curso.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [DirectorDoCursoComponent]
 })
 export class DirectorDoCursoModule { }
