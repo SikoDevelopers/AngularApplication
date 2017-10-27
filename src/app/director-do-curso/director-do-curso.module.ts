5cb31fc5cde106e2467e449430fade19b90c63e4
AlfredoSebastiao <stiaoalfredo@gmail.com>
2017-10-27 12:40:58 +0200

    Barra e side nav imported

--- src/app/director-do-curso/director-do-curso.module.ts
+++ src/app/director-do-curso/director-do-curso.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DirectorDoCursoComponent } from './director-do-curso.component';
+import {BarraDoTopoEstModule} from "./barra-do-topo-est/barra-do-topo-est.module";
+import {SidenavComponent} from "./sidenav/sidenav.component";
 
 @NgModule({
   imports: [
-    CommonModule
+      CommonModule,
+      BarraDoTopoEstModule
   ],
-  declarations: [DirectorDoCursoComponent]
+  declarations: [
+      DirectorDoCursoComponent,
+      SidenavComponent
+  ]
 })
 export class DirectorDoCursoModule { }
