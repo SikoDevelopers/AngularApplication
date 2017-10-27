ec473a3171dd01353544a3387b942347c360ba11
AlfredoSebastiao <stiaoalfredo@gmail.com>
2017-10-27 13:07:57 +0200

    conteudo component

--- src/app/director-do-curso/director-do-curso.module.ts
+++ src/app/director-do-curso/director-do-curso.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { DirectorDoCursoComponent } from './director-do-curso.component';
 import {BarraDoTopoEstModule} from "./barra-do-topo-est/barra-do-topo-est.module";
 import {SidenavComponent} from "./sidenav/sidenav.component";
+import { ConteudoComponent } from './conteudo/conteudo.component';
 
 @NgModule({
   imports: [
@@ -11,7 +12,8 @@ import {SidenavComponent} from "./sidenav/sidenav.component";
   ],
   declarations: [
       DirectorDoCursoComponent,
-      SidenavComponent
+      SidenavComponent,
+      ConteudoComponent
   ]
 })
 export class DirectorDoCursoModule { }
