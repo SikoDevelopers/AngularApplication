2eb67299950af02eb094eeed73093d14bc19647b
erciliomarques <erciliomarquesmanhica@gmail.com>
2017-10-28 10:08:55 +0200

    Submissao 17

--- src/app/estudante/trabalhos-submetidos/trabalhos-submetidos.component.ts
+++ src/app/estudante/trabalhos-submetidos/trabalhos-submetidos.component.ts
@@ -1,9 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
-import {UserService} from "../../service/user.service";
-import {TrabalhoService} from "../../service/trabalho.service";
-import {FicheirosTrabalhoService} from "../../service/ficheiros-trabalho.service";
-import {EstudanteService} from "../../service/estudante.service";
-import {FicheiroTrabalhoEstadoFicheiroService} from "../../service/ficheiro-trabalho-estado-ficheiro.service";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-trabalhos-submetidos',
@@ -12,117 +7,9 @@ import {FicheiroTrabalhoEstadoFicheiroService} from "../../service/ficheiro-trab
 })
 export class TrabalhosSubmetidosComponent implements OnInit {
 
-    ficheiros: Array<any>
-    user;
-    estado;
-    estudante;
-    trabalho;
-    @Input() modal: any;
-  constructor(private userService: UserService,
-              private trabalhoService: TrabalhoService,
-              private ficheiroService: FicheirosTrabalhoService,
-              private estudanteService: EstudanteService,
-              private estadoFicheiroService : FicheiroTrabalhoEstadoFicheiroService
-
-              ) { }
+  constructor() { }
 
   ngOnInit() {
-
-      this.getUser();
-
-      // alert(this.user.id);
-      // alert('estudante'+this.estudante.id);
-      // alert('trabalho'+this.trabalho.id);
   }
 
-    getUser(){
-        const token = localStorage.getItem('token');
-
-
-        this.userService.logado(token).subscribe(
-            resultado=>{
-                this.user = resultado;
-
-            },
-            error2 => {
-            },
-            ()=>{
-                console.log('user retrivied');
-                this.getEstudante();
-
-            }
-        );
-    }
-
-
-    getEstudante(){
-                let estudante: any = 'hhhh';
-
-                this.estudanteService.getEstudantesByIdUser(this.user.id).subscribe(
-                    result=>{
-                        estudante = result['estudante'][0];
-
-                        console.log(estudante);
-                    },
-                    (error)=>{
-                        console.log(error)
-                    },
-                    ()=>{
-                        this.estudante = estudante;
-                        console.log(estudante);
-                        this.getTrabalhoEstudante(estudante.id);
-
-                    }
-                )
-            }
-
-
-            getTrabalhoEstudante(id){
-
-                this.trabalhoService.getTrabalhosEstudante(id).subscribe(
-                    resul=>{
-                        this.trabalho = resul['trabalho'];
-
-                        console.log(this.trabalho);
-
-
-
-                    },
-                    (error)=>{
-                        console.log(error);
-                    },
-                    ()=>{
-                        this.ficheiros= this.trabalho.ficheiros_trabalhos;
-                        console.log('files '+this.ficheiros);
-                        this.getEstadoFicheiro(this.trabalho.id);
-
-                    }
-                )
-            }
-
-            getEstadoFicheiro(id){
-                this.estadoFicheiroService.getEstadoFicheiro(id).subscribe(
-                    resultado=>{
-                        this.estado = resultado.estado;
-                    },
-                    (error)=>{
-                        console.log(error);
-                    },
-                    ()=>{
-                        console.log('state retrieved');
-                    }
-                )
-            }
-
-    verFicheiro(caminho){
-                // window.location.href="http://127.0.0.1:8000/api/display/"+caminho;
-        window.open("http://127.0.0.1:8000/api/display/"+caminho,'_blank');
-    }
-
-    getModal(event){
-        this.modal= event;
-    }
-    onMostrarModal(){
-        this.modal.show();
-    }
 }
