30302d01c52237384c24b6fa3a147c48db36ed57
herquiloidehele <herquiloide@gmail.com>
2017-10-29 03:46:29 +0200

    validacoes para dicionar particiantes

--- src/app/comissao-cientifica/conteudo/trabalhos/trabalhos-detalhes/trabalhos-detalhes.component.ts
+++ src/app/comissao-cientifica/conteudo/trabalhos/trabalhos-detalhes/trabalhos-detalhes.component.ts
@@ -1,6 +1,7 @@
 import {Component, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
 import {TrabalhoService} from '../../../../service/trabalho.service';
 import {HttpErrorResponse} from '@angular/common/http';
+import {forEach} from '@angular/router/src/utils/collection';
 
 
 @Component({
@@ -13,11 +14,11 @@ export class TrabalhosDetalhesComponent implements OnInit {
     @ViewChild('modal') modal;
     @Output() output = new EventEmitter();
     @Input() trabalho: any;
-    @Input() docentes: any;
+    @Input() docentes = [];
     @Input() modalParticipantes: any;
     @Input() docentesdicionados = [];
 
-  constructor() {
+  constructor(private trabalhoService: TrabalhoService) {
 
   }
 
@@ -66,13 +67,98 @@ export class TrabalhosDetalhesComponent implements OnInit {
     }
 
     salvarAlteracoes(){
-        console.log("Devem ser adicionados os seguintes docentes");
-        console.log(this.docentesdicionados);
-        console.log("Para este trabalho");
-        console.log(this.trabalho);
+        // console.log("Devem ser adicionados os seguintes docentes");
+        // console.log(this.docentesdicionados);
+        // console.log("Para este trabalho");
+        // console.log(this.trabalho);
+
+        if(this.verificarDuplicacoes() && this.verificarElementosDuplicados(this.docentesdicionados))
+            if(this.verificarFuncoesDuplicadas(this.docentes, this.docentesdicionados))
+                this.salvarParticipantes();
+            else
+                alert('Existem participantes com a mesma funcao');
+        else
+            alert('Existem docentes duplicados');
 
     }
 
 
+
+
+
+    salvarParticipantes(){
+        this.trabalhoService.adicinarParticipantes(this.trabalho, this.docentesdicionados).subscribe(
+            (resultado: Response) =>{
+                console.log(resultado);
+            },
+            (erros: HttpErrorResponse)=>{
+                console.error(erros);
+            },
+            ()=> {
+                alert("Participantes adicionados com  sucesso");
+                this.modal.hide();
+                this.docentesdicionados = [];
+            }
+        );
+    }
+
+
+    /**
+     * Verifica se existe alguma duplicacao de docentes e participantes
+     * @returns {boolean}
+     */
+    verificarDuplicacoes(): boolean{
+        let isDuplicated: boolean = true;
+        let docentesAdicionais = this.docentesdicionados;
+        this.docentes.forEach(function(docente){
+            docentesAdicionais.forEach(function (docenteAdicionado) {
+                if(docenteAdicionado['id'] == docente['id'])
+                    isDuplicated = false;
+            });
+        });
+
+        return isDuplicated;
+    }
+
+
+    /**
+     * verifica se existem participantes duplicados
+     * @returns {boolean}
+     */
+    verificarElementosDuplicados(array): boolean{
+        let isDuplicated: boolean = true;
+        array.forEach(function (elemento, index) {
+            if(array.indexOf(elemento, index+1) > -1)
+                isDuplicated = false
+        });
+
+        return isDuplicated;
+    }
+
+
+    /**
+     * Verifica se existe funcoes repetidas no docentes
+     * @param docentes
+     * @param participantes
+     * @returns {boolean}
+     */
+    verificarFuncoesDuplicadas(docentes, participantes){
+        let funcoes = [];
+        docentes.forEach(function (docente) {
+            funcoes.push(docente.funcao.designacao);
+        });
+
+        participantes.forEach(function (participante) {
+           funcoes.push(participante.funcao.designacao);
+        });
+
+        return this.verificarElementosDuplicados(funcoes);
+    }
+
+
+    fecharModal(evento){
+        this.modal.hide();
+        this.docentesdicionados = [];
+    }
 
 }
