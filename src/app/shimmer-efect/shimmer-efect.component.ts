7e6a507b226244496eeadfc663d2de464cd452f2
herquiloidehele <herquiloide@gmail.com>
2017-11-01 02:18:12 +0200

    shemmer efect

--- src/app/shimmer-efect/shimmer-efect.component.ts
+++ src/app/shimmer-efect/shimmer-efect.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {CompleterData, CompleterService} from 'ng2-completer';
 
 @Component({
@@ -9,26 +9,10 @@ import {CompleterData, CompleterService} from 'ng2-completer';
 export class ShimmerEfectComponent implements OnInit {
 
 
+    constructor(){}
+    dataActual = new Date();
+
   ngOnInit() {
   }
-
-
-    protected searchStr: string;
-    protected captain: string;
-    protected dataService: CompleterData;
-    protected searchData = [
-        { color: 'red', value: '#f00' },
-        { color: 'green', value: '#0f0' },
-        { color: 'blue', value: '#00f' },
-        { color: 'cyan', value: '#0ff' },
-        { color: 'magenta', value: '#f0f' },
-        { color: 'yellow', value: '#ff0' },
-        { color: 'black', value: '#000' }
-    ];
-    protected captains = ['James T. Kirk', 'Benjamin Sisko', 'Jean-Luc Picard', 'Spock', 'Jonathan Archer', 'Hikaru Sulu', 'Christopher Pike', 'Rachel Garrett' ];
-
-    constructor(private completerService: CompleterService) {
-        this.dataService = completerService.local(this.searchData, 'color', 'color');
-    }
 
 }
