f3d9a04f58183d852d7633bbb5c8223becdcd261
AlfredoSebastiao <stiaoalfredo@gmail.com>
2017-11-01 10:34:23 +0200

    metodo para ir buscar trabalhos criado no componente detalhes

--- src/app/publico-geral/lista-de-trabalhos/trabalho/ler-mais-trabalhos/ler-mais-trabalhos.component.ts
+++ src/app/publico-geral/lista-de-trabalhos/trabalho/ler-mais-trabalhos/ler-mais-trabalhos.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {Subscription} from "rxjs/Subscription";
+import {TrabalhoService} from "../../../../service/trabalho.service";
 
 @Component({
   selector: 'app-ler-mais-trabalhos',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LerMaisTrabalhosComponent implements OnInit {
 
-  constructor() { }
+  subscription: Subscription;
+  id:number;
+  trabalho: any;
+  constructor(private _routerActived:  ActivatedRoute, private _trabalhoService: TrabalhoService) { }
 
   ngOnInit() {
+      this.subscription = this._routerActived.params.subscribe(
+          (params: any) =>{
+              this.id = params['id-trabalho'];
+
+              this.getTrabalho(this.id);
+
+          }
+      );
+  }
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
+  }
+
+
+  getTrabalho(id:number){
+      this._trabalhoService
   }
 
 }
