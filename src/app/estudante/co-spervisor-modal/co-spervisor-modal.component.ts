1cdf9f82c7d8cbaac661bdd0bee81a67094f516c
erciliomarques <erciliomarquesmanhica@gmail.com>
2017-11-11 22:32:33 +0200

    working with co-supervisor registration  modal

---
+++ src/app/estudante/co-spervisor-modal/co-spervisor-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-co-spervisor-modal',
+  templateUrl: './co-spervisor-modal.component.html',
+  styleUrls: ['./co-spervisor-modal.component.scss']
+})
+export class CoSpervisorModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
